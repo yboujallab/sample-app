f43246b9add9c8b5396f098e1b66e4b64396859b
youssef boujallab <youssef.boujallab&gmail.com>
2014-11-10 13:50:32 +0100

    Inscription utilisateur finie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,24 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   	@titre = "Inscription"
   end
    def show
     @user = User.find(params[:id])
     @titre = @user.nom
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    	flash[:success] = "Bienvenue dans l'Application Exemple!"
+    	redirect_to @user
+      # Traite un succès d'enregistrement.
+    else
+      @titre = "Inscription"
+      render 'new'
+    end
+  end
+  def user_params
+    params.require(:user).permit(:nom, :email, :password, :password_confirmation)
   end
 end
